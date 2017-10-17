2d159bf0b23197bddd19137e1d29a7feb2a8411a
Braden Harrelson <bradentharrelson@gmail.com>
2017-10-17 12:10:11 -0500

    Add files via upload

---
+++ 272.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string line;
+	bool flag = true;
+	while(getline(cin, line)){
+		for (int i = 0; i < line.length(); i++){
+			if (line[i] == '"' && flag){
+				line.replace(i, 1, "``");
+				flag = !flag;
+			}
+			else if(line[i] == '"' && !flag){
+				line.replace(i, 1, "''");
+				flag = !flag;
+			}
+		}
+		cout << line << '\n';
+	}
+	return 0;
+}
